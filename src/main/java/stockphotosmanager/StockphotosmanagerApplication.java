b925ba509c14bb47f9466d9914aa3eb54cd48fe9
Quique <elamas@gmail.com>
2019-09-30 13:41:41 +0000

    version inicial

---
+++ src/main/java/stockphotosmanager/StockphotosmanagerApplication.java
@@ -0,0 +1,13 @@
+package stockphotosmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockphotosmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockphotosmanagerApplication.class, args);
+	}
+
+}
